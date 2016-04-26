e1f4485a1b9929620703e19599b929b9f9db70a9
Nick Frost <nickfrostatx@gmail.com>
2016-04-26 10:46:31 -0700

    Test more

--- test_bencode.py
+++ test_bencode.py
@@ -25,3 +25,36 @@ def test_dump_bad_data():
         with pytest.raises(ValueError) as exc:
             dumps(data)
         assert 'not supported' in str(exc)
+
+
+def test_load_good():
+    assert loads(b'i0e') == 0
+    assert loads(b'i123e') == 123
+    assert loads(b'0:') == b''
+    assert loads(b'6:abcdef') == b'abcdef'
+    assert loads(b'10:abcdefghij') == b'abcdefghij'
+    assert loads(b'le') == []
+    assert loads(b'llleee') == [[[]]]
+    assert loads(b'li1ei2e3:abce') == [1, 2, b'abc']
+    assert loads(b'de') == {}
+    assert loads(b'd3:abcli1ei2ei3ee3:key5:valuee') == \
+        {b'key': b'value', b'abc': [1, 2, 3]}
+
+
+def test_load_bad():
+    exceptions = {
+        'loads expects bytes': [u'abc'],
+        'Unexpected data': [b'i10eX', b'3:abcX', b'leX', b'deX'],
+        'Unexpected end of stream': [b'', b'0', b'1:', b'4:abc', b'd3:abc',
+                                     b'i', b'i123', b'123'],
+        'Expected terminator': [b'l', b'lllee', b'll0:d', b'dd', b'dl'],
+        'Expected string key': [b'di123e', b'dde'],
+        'Unexpected token': [b'X', b'lX', b'12X', b'iXXXe', b'i-1e', b'i0Xe'],
+        'Leading zeros are not allowed': [b'i00e', b'i01e'],
+    }
+
+    for msg in exceptions:
+        for data in exceptions[msg]:
+            with pytest.raises(ValueError) as exc:
+                loads(data)
+            assert msg in exc.value.args[0]
