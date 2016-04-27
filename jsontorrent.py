eb32d4d49b63e252a3f1d4b7753dfe0e042fbe45
Nick Frost <nickfrostatx@gmail.com>
2016-04-27 13:54:36 -0700

    JSON fromat, don't hex data

--- jsontorrent.py
+++ jsontorrent.py
@@ -1,11 +1,34 @@
 import bencode
+import json
 import sys
 import util
 
 
+def pretty_format(data):
+    return json.dumps(decode_deep(data), sort_keys=True, indent=4,
+                      separators=(',', ': '))
+
+
+def decode_deep(data):
+    if isinstance(data, bytes):
+        try:
+            return data.decode('ascii')
+        except ValueError:
+            return '<%d bytes of raw binary>' % len(data)
+    elif isinstance(data, int):
+        return data
+    elif isinstance(data, list):
+        return [decode_deep(v) for v in list]
+    elif isinstance(data, dict):
+        obj = {}
+        for k in data:
+            obj[decode_deep(k)] = decode_deep(data[k])
+        return obj
+
+
 def main(argv):
     data = bencode.load(open(sys.argv[1], 'rb'))
-    print(util.pretty_format(data))
+    print(pretty_format(data))
     return 0
 
 
